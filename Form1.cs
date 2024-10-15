f507a83e1ee3db4ee8248d5c5141a568507bd237
rie4015 <rie4015@yandex.ru>
2024-10-16 02:24:41 +0500

    done

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ namespace WindowsFormsApp5
         private int[] array = { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
         private int[] array1 = { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
         private int[] array2 = { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
-        private Semaphore semaphore1 = new Semaphore(1, 1); // Семафор для запуска второго потока
+        private Semaphore semaphore1 = new Semaphore(0, 2); // Семафор для запуска второго потока
         private Semaphore semaphore2 = new Semaphore(0, 1); // Семафор для завершения обоих потоков
-        private Semaphore semaphore3 = new Semaphore(0, 2);
+        private Semaphore semaphore3 = new Semaphore(0, 1);
         private bool useCriticalSections; // Флаг для использования критических секций
         private object lockObject = new object(); // Объект блокировки
 
@@ -31,49 +31,57 @@ namespace WindowsFormsApp5
             array2 = new int[] { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
 
             txtGeneratedArray.Text = string.Join(", ", array);
+            semaphore1.Release();
+            semaphore1.Release();
         }
 
 
         private void btnSortArray_Click(object sender, EventArgs e)
         {
-            
-            //txtSortedArray.Text += "Сортировка пузырьком (оригинальный массив): " + string.Join(", ", array) + Environment.NewLine;
-            useCriticalSections = chkUseCriticalSections.Checked;
 
+            useCriticalSections = chkUseCriticalSections.Checked;
 
             Thread bubbleSortThread = new Thread(BubbleSort);
             Thread selectionSortThread = new Thread(SelectionSort);
+            Thread textThread = new Thread(textBoxMassage);
+
 
             txtSortedArray.Clear();
+
+
+
+            // Запускаем оба потока
+
             bubbleSortThread.Start();
+  
             selectionSortThread.Start();
+            textThread.Start();
 
+            // Главный поток ждет завершения обоих потоков через semaphore3
 
-            //currentThread.Start();
-            semaphore3.WaitOne();
-            semaphore3.WaitOne();
-            textBoxMassage();
-
-
-            //semaphore3.Release();
+ 
 
         }
         private void textBoxMassage()
         {
+            semaphore2.WaitOne();
+            semaphore3.WaitOne();
+            //if (!useCriticalSections) Invoke(new Action(() => txtSortedArray.Text += "Сортировка пузырьком (массив в потоке) : " + string.Join(", ", array) + Environment.NewLine));
+            Invoke(new Action(() => txtSortedArray.Text += "Сортировка пузырьком (оригинальный массив): " + string.Join(", ", array1) + Environment.NewLine));
+            //if (!useCriticalSections) Invoke(new Action(() => txtSortedArray.Text += "Сортировка методом выбора максимума (массив в потоке) : " + string.Join(", ", array) + Environment.NewLine));
+            Invoke(new Action(() => txtSortedArray.Text += "Сортировка методом выбора максимума (оригинальный массив): " + string.Join(", ", array2) + Environment.NewLine));
 
-            txtSortedArray.Text += "Сортировка пузырьком (оригинальный массив)00: " + string.Join(", ", array) + Environment.NewLine;
-            txtSortedArray.Text += "Сортировка пузырьком (оригинальный массив)11: " + string.Join(", ", array1) + Environment.NewLine;
-            txtSortedArray.Text += "Сортировка пузырьком (оригинальный массив)22: " + string.Join(", ", array2) + Environment.NewLine;
-            semaphore3.Release();
         }
 
         //Метод сортировки пузырьком
 
         private void BubbleSort()
         {
+
             semaphore1.WaitOne();
-            Invoke(new Action(() => txtSortedArray.Text += "Поток 1 : " + string.Join(", ", array1) + Environment.NewLine));
-            if (useCriticalSections) lock (lockObject)
+
+            if (useCriticalSections) 
+                lock (lockObject)
                 {
                     for (int i = 0; i < 10 - 1; i++)
                     {
@@ -92,11 +100,9 @@ namespace WindowsFormsApp5
                             Thread.Sleep(100);
                         }
                     }
-                    semaphore2.Release();
-                    semaphore3.Release();
 
-                    Invoke(new Action(() => txtSortedArray.Text += "Сортировка пузырьком (массив в потоке) : " + string.Join(", ", array1) + Environment.NewLine));
-                    Invoke(new Action(() => txtSortedArray.Text += "Сортировка пузырьком (оригинальный массив): " + string.Join(", ", array) + Environment.NewLine));
+
+                   
                 }
             else
             {
@@ -117,21 +123,22 @@ namespace WindowsFormsApp5
                         Thread.Sleep(100);
                     }
                 }
-                Invoke(new Action(() => txtSortedArray.Text += "Сортировка пузырьком (массив в потоке) : " + string.Join(", ", array1) + Environment.NewLine));
-                Invoke(new Action(() => txtSortedArray.Text += "Сортировка пузырьком (оригинальный массив): " + string.Join(", ", array) + Environment.NewLine));
-                semaphore2.Release();
-                semaphore3.Release();
+
+
             }
-            
+            semaphore2.Release();
+
 
         }
 
         // Метод сортировки выбором иаксимума
         private void SelectionSort()
         {
-            semaphore2.WaitOne();
-            Invoke(new Action(() => txtSortedArray.Text += "Поток 2 (массив в потоке) : " + string.Join(", ", array1) + Environment.NewLine));
-            if (useCriticalSections) lock (lockObject)
+
+            semaphore1.WaitOne();
+
+            if (useCriticalSections) 
+                lock (lockObject)
                 {
 
                     for (int i = 0; i < 10; i++)
@@ -153,14 +160,8 @@ namespace WindowsFormsApp5
                         array2[i] = temp;
                     }
                     
-                    semaphore3.Release();
 
-
-
-
-                    Invoke(new Action(() => txtSortedArray.Text += "Сортировка методом выбора максимума (массив в потоке) : " + string.Join(", ", array2) + Environment.NewLine));
-                    
-                    Invoke(new Action(() => txtSortedArray.Text += "Сортировка методом выбора максимума (оригинальный массив): " + string.Join(", ", array) + Environment.NewLine));
+                   
                 }
             else
             {
@@ -182,13 +183,13 @@ namespace WindowsFormsApp5
                     array2[maxIdx] = array2[i];
                     array2[i] = temp;
                 }
-                Invoke(new Action(() => txtSortedArray.Text += "Сортировка методом выбора максимума (массив в потоке) : " + string.Join(", ", array2) + Environment.NewLine));
-                Invoke(new Action(() => txtSortedArray.Text += "Сортировка методом выбора максимума (оригинальный массив): " + string.Join(", ", array) + Environment.NewLine));
-                
-                semaphore3.Release();
-            }
-           
 
+                
+                
+            }
+
+
+            semaphore3.Release();
         }
       
 
