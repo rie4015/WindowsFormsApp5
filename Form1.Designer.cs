e5bac9794e7d1a8ed99fca848da64a60858ca45d
rie4015 <rie4015@yandex.ru>
2024-10-15 23:34:01 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerateArray = new System.Windows.Forms.Button();
+            this.btnSortArray = new System.Windows.Forms.Button();
+            this.chkUseCriticalSections = new System.Windows.Forms.CheckBox();
+            this.txtGeneratedArray = new System.Windows.Forms.TextBox();
+            this.txtSortedArray = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerateArray
+            // 
+            this.btnGenerateArray.BackColor = System.Drawing.Color.Gold;
+            this.btnGenerateArray.Location = new System.Drawing.Point(605, 235);
+            this.btnGenerateArray.Name = "btnGenerateArray";
+            this.btnGenerateArray.Size = new System.Drawing.Size(176, 45);
+            this.btnGenerateArray.TabIndex = 0;
+            this.btnGenerateArray.Text = "Инициализировать массив";
+            this.btnGenerateArray.UseVisualStyleBackColor = false;
+            this.btnGenerateArray.Click += new System.EventHandler(this.btnGenerateArray_Click_1);
+            // 
+            // btnSortArray
+            // 
+            this.btnSortArray.BackColor = System.Drawing.Color.Lime;
+            this.btnSortArray.Location = new System.Drawing.Point(787, 235);
+            this.btnSortArray.Name = "btnSortArray";
+            this.btnSortArray.Size = new System.Drawing.Size(176, 45);
+            this.btnSortArray.TabIndex = 1;
+            this.btnSortArray.Text = "Выполнить сортировку";
+            this.btnSortArray.UseVisualStyleBackColor = false;
+            this.btnSortArray.Click += new System.EventHandler(this.btnSortArray_Click);
+            // 
+            // chkUseCriticalSections
+            // 
+            this.chkUseCriticalSections.AutoSize = true;
+            this.chkUseCriticalSections.BackColor = System.Drawing.Color.Transparent;
+            this.chkUseCriticalSections.Location = new System.Drawing.Point(22, 84);
+            this.chkUseCriticalSections.Name = "chkUseCriticalSections";
+            this.chkUseCriticalSections.Size = new System.Drawing.Size(205, 17);
+            this.chkUseCriticalSections.TabIndex = 2;
+            this.chkUseCriticalSections.Text = "Использовать критические секции";
+            this.chkUseCriticalSections.UseVisualStyleBackColor = false;
+            // 
+            // txtGeneratedArray
+            // 
+            this.txtGeneratedArray.Location = new System.Drawing.Point(16, 22);
+            this.txtGeneratedArray.Name = "txtGeneratedArray";
+            this.txtGeneratedArray.Size = new System.Drawing.Size(529, 20);
+            this.txtGeneratedArray.TabIndex = 4;
+            this.txtGeneratedArray.Text = "Оригинальный массив";
+            // 
+            // txtSortedArray
+            // 
+            this.txtSortedArray.Location = new System.Drawing.Point(16, 59);
+            this.txtSortedArray.Multiline = true;
+            this.txtSortedArray.Name = "txtSortedArray";
+            this.txtSortedArray.Size = new System.Drawing.Size(529, 159);
+            this.txtSortedArray.TabIndex = 5;
+            this.txtSortedArray.Text = "Отсортированные массивы";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.chkUseCriticalSections);
+            this.panel1.Location = new System.Drawing.Point(605, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(358, 185);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.txtSortedArray);
+            this.panel2.Controls.Add(this.txtGeneratedArray);
+            this.panel2.Location = new System.Drawing.Point(12, 44);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(570, 236);
+            this.panel2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 322);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnSortArray);
+            this.Controls.Add(this.btnGenerateArray);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateArray;
+        private System.Windows.Forms.Button btnSortArray;
+        private System.Windows.Forms.CheckBox chkUseCriticalSections;
+        private System.Windows.Forms.TextBox txtGeneratedArray;
+        private System.Windows.Forms.TextBox txtSortedArray;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+
+        private int[] array = { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
+        private int[] array1 = { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
+        private int[] array2 = { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
+        private Semaphore semaphore1 = new Semaphore(0, 1); // Семафор для запуска второго потока
+        private Semaphore semaphore2 = new Semaphore(0, 1); // Семафор для завершения обоих потоков
+        private bool useCriticalSections; // Флаг для использования критических секций
+        private object lockObject = new object(); // Объект блокировки
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnGenerateArray_Click_1(object sender, EventArgs e)
+        {
+
+            array = new int[] { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
+            array1 = new int[] { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
+            array2 = new int[] { 8, 3, 2, 1, 4, 5, 6, 7, 0, 9 };
+
+            txtGeneratedArray.Text = string.Join(", ", array);
+        }
+
+
+        private void btnSortArray_Click(object sender, EventArgs e)
+        {
+
+            useCriticalSections = chkUseCriticalSections.Checked;
+
+
+            Thread bubbleSortThread = new Thread(BubbleSort);
+            Thread selectionSortThread = new Thread(SelectionSort);
+
+            txtSortedArray.Clear();
+
+
+            bubbleSortThread.Start();
+            if (useCriticalSections) semaphore1.WaitOne();
+            selectionSortThread.Start();
+
+        }
+
+
+        //Метод сортировки пузырьком
+
+        private void BubbleSort()
+        {
+            if (useCriticalSections) Monitor.Enter(lockObject);
+
+            for (int i = 0; i < 10 - 1; i++)
+            {
+                for (int j = 0; j < 10 - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        array1[j] = array1[j + 1];
+                        array1[j + 1] = temp;
+
+
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+
+            if (useCriticalSections) semaphore1.Release();
+            if (!useCriticalSections)
+            {
+                Invoke(new Action(() => txtSortedArray.Text += "Сортировка пузырьком (массив в потоке) : " + string.Join(", ", array1) + Environment.NewLine));
+            }
+            Invoke(new Action(() => txtSortedArray.Text += "Сортировка пузырьком (оригинальный массив): " + string.Join(", ", array) + Environment.NewLine));
+
+            if (useCriticalSections) Monitor.Exit(lockObject);
+        }
+
+        // Метод сортировки выбором иаксимума
+        private void SelectionSort()
+        {
+            if (useCriticalSections) Monitor.Enter(lockObject); // Вход в критическую секцию
+
+            for (int i = 0; i < 10; i++)
+            {
+                int maxIdx = i;
+                for (int j = i + 1; j < 10; j++)
+                {
+                    if (array[j] > array[maxIdx])
+                    {
+                        maxIdx = j;
+
+                    }
+                    Thread.Sleep(100);
+                }
+                int temp = array[maxIdx];
+                array[maxIdx] = array[i];
+                array[i] = temp;
+                array2[maxIdx] = array2[i];
+                array2[i] = temp;
+            }
+
+            if (useCriticalSections) semaphore2.Release(); // Разрешение для завершения работы
+            if (!useCriticalSections)
+            {
+                Invoke(new Action(() => txtSortedArray.Text += "Сортировка методом выбора максимума (массив в потоке) : " + string.Join(", ", array2) + Environment.NewLine));
+            }
+            Invoke(new Action(() => txtSortedArray.Text += "Сортировка методом выбора максимума (оригинальный массив): " + string.Join(", ", array) + Environment.NewLine));
+
+            if (useCriticalSections) Monitor.Exit(lockObject); // Выход из критической секции
+        }
+
+    }
+
+}
